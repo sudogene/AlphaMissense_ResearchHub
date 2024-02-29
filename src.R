03593a100ae7afee8b824d589a490eb2522a721b
Andy Wu <49007969+sudogene@users.noreply.github.com>
2024-02-29 14:30:52 +0800

    Update src.R

--- src.R
+++ src.R
@@ -6,7 +6,7 @@ collect_id <- function(index, output_file, max_size = 1000000) {
     # chunk & pos are fixed arguments of the callback function
     function(chunk, pos) {
         # ids is a global vector hence manipulated using <<-
-        ids <<- c(ids, chunk[[index]])
+        ids <- c(ids, chunk[[index]])
         ids <<- unique(ids)
         # if ids grow too large, write the partial result and reset the vector
         # there is a small chance a repeated value will slip through if the
@@ -25,11 +25,21 @@ ids <- c()
 output_file <- "AM_uniprot_id.tsv"
 read_tsv_chunked("AlphaMissense_hg38.tsv.gz",
                  DataFrameCallback$new(collect_id(6, output_file)),
-                 comment = "#", chunk_size = 1000, show_col_types = F)
+                 comment = "#", chunk_size = 10000, show_col_types = F)
 # write the last chunk to output
 write.table(as_tibble(ids), output_file, sep = "\t",
             col.names = F, row.names = F, quote = F, append = T)
 # free up memory
+rm(ids)
+
+# --- Collect IDs from AlphaMissense_aa_substitutions.tsv.gz
+ids <- c()
+output_file <- "AM_aa_uniprot_id.tsv"
+read_tsv_chunked("AlphaMissense_aa_substitutions.tsv.gz",
+                 DataFrameCallback$new(collect_id(1, output_file)),
+                 comment = "#", chunk_size = 10000, show_col_types = F)
+write.table(as_tibble(ids), output_file, sep = "\t",
+            col.names = F, row.names = F, quote = F, append = T)
 rm(ids)
 
 # --- Collect IDs from AlphaMissense_isoforms_hg38.tsv.gz
@@ -37,7 +47,7 @@ ids <- c()
 output_file <- "AM_isoform_ens_id.tsv"
 read_tsv_chunked("AlphaMissense_isoforms_hg38.tsv.gz",
                  DataFrameCallback$new(collect_id(6, output_file)),
-                 comment = "#", chunk_size = 1000, show_col_types = F)
+                 comment = "#", chunk_size = 10000, show_col_types = F)
 write.table(as_tibble(ids), output_file, sep = "\t",
             col.names = F, row.names = F, quote = F, append = T)
 rm(ids)
@@ -47,39 +57,49 @@ ids <- c()
 output_file <- "AM_isoform_aa_ens_id.tsv"
 read_tsv_chunked("AlphaMissense_isoforms_aa_substitutions.tsv.gz",
                  DataFrameCallback$new(collect_id(1, output_file)),
-                 comment = "#", chunk_size = 1000, show_col_types = F)
+                 comment = "#", chunk_size = 10000, show_col_types = F)
 write.table(as_tibble(ids), output_file, sep = "\t",
             col.names = F, row.names = F, quote = F, append = T)
 rm(ids)
 
 
-# --- Querying the IDs
+# --- Harmonizing the IDs
 ensembl_canonical <- read_tsv("canonical_transcripts.tsv", col_names = T)
 am_uniprot <- unique(read_tsv("AM_uniprot_id.tsv", col_names = F))
+am_aa_uniprot <- unique(read_tsv("AM_aa_uniprot_id.tsv", col_names = F))
 am_iso_ens <- unique(read_tsv("AM_isoform_ens_id.tsv", col_names = F))
 ### am_iso_aa is a superset of am_iso_ens, we don't need to use am_iso_ens.
 am_iso_aa_ens <- unique(read_tsv("AM_isoform_aa_ens_id.tsv", col_names = F))
-idmapping <- read_tsv("idmapping_1.tsv.gz")
-### idmapping obtained from Uniprot tool, 49328 hits.
+idmapping1 <- read_tsv("idmapping_1.tsv.gz")
+idmapping2 <- read_tsv("idmapping_2.tsv.gz")
+### idmapping1 obtained from Uniprot tool (AM_uniprot_id.tsv), 49328 hits.
+### idmapping2 obtained from Uniprot tool, (AM_aa_uniprot_id.tsv), 50550 hits.
 
-idmapping <- dplyr::rename(idmapping, "uniprot_ID" = "From", "ID" = "To")
-am_mapped_to_ensembl_canonical <- ensembl_canonical %>% inner_join(idmapping, by = "ID")
+idmapping1 <- dplyr::rename(idmapping1, "uniprot_ID" = "From", "ID" = "To")
+idmapping2 <- dplyr::rename(idmapping2, "uniprot_ID" = "From", "ID" = "To")
+idmapping1_canonical <- ensembl_canonical %>% inner_join(idmapping1, by = "ID")
+idmapping2_canonical <- ensembl_canonical %>% inner_join(idmapping2, by = "ID")
+am_mapped_to_ensembl_canonical <- bind_rows(idmapping1_canonical, idmapping2_canonical) %>% distinct()
+### Combine all IDs from AM_uniprot and AM_aa_uniprot, 18407 IDs.
+
 am_iso_aa_ens <- dplyr::rename(am_iso_aa_ens, "ID" = "X1")
-missing_from_am <- ensembl_canonical %>% filter(!ID %in% idmapping$ID)
-### 1569 IDs in ensembl canonical but not in the main AM.
+missing_from_am <- ensembl_canonical %>% filter(!ID %in% am_mapped_to_ensembl_canonical$ID)
+print(missing_from_am)
+### 1288 IDs in ensembl canonical but not in the main AM.
 print(missing_from_am %>% filter(ID %in% am_iso_aa_ens$ID))
-### 862 out of 1569 found in AM_iso_aa
+### 752 out of 1288 found in AM_iso_aa.
 missing_from_am <- missing_from_am %>% mutate(ID2 = sub("\\.\\d+$", "", ID))
 am_iso_aa_ens <- am_iso_aa_ens %>% mutate(ID2 = sub("\\.\\d+$", "", ID))
 print(missing_from_am %>% filter(ID2 %in% am_iso_aa_ens$ID2))
-### 1181 out of 1569 found in AM_iso_aa if using stable IDs without version.
+### 1004 out of 1288 found in AM_iso_aa if using stable IDs without version.
 ids_to_add <- missing_from_am %>% filter(ID2 %in% am_iso_aa_ens$ID2)
 ids_to_add <- ids_to_add %>% mutate(uniprot_ID = "") %>% select("ID", "uniprot_ID")
 
 am_mapped_to_ensembl_canonical <- rows_insert(am_mapped_to_ensembl_canonical, ids_to_add)
 am_mapped_to_ensembl_canonical <- am_mapped_to_ensembl_canonical %>% mutate(ID2 = sub("\\.\\d+$", "", ID))
-### Combine all IDs
+### Combine all IDs, 19411 IDs in total.
 
+# --- Filtering the IDs
 # Ensembl IDs that match to >1 Uniprot ID
 multiple_match <- am_mapped_to_ensembl_canonical %>% 
     group_by(ID) %>%
@@ -87,21 +107,23 @@ multiple_match <- am_mapped_to_ensembl_canonical %>%
     summarise(uniprot_IDs = list(unique(uniprot_ID))) %>%
     unnest(cols = uniprot_IDs)
 print(multiple_match)
-### 5 IDs flagged. How did OP decide which uniprot_ID to use for each of these?
-obsolete_ids <- c("Q32Q52", "Q8IXS6", "Q5VZT2", "Q9UPP5", "Q6ZW33")
+### 6 IDs flagged.
+obsolete_ids <- c("A0A140G945", "Q32Q52", "Q8IXS6", "Q5VZT2", "Q9UPP5", "Q6ZW33")
+### Obsolete ids identified from looking up Uniprot entries.
 am_mapped_to_ensembl_canonical <- am_mapped_to_ensembl_canonical %>%
     filter(!uniprot_ID %in% obsolete_ids)
 
 # Uniprot IDs that did not map using Uniprot tool
+idmapping <- bind_rows(idmapping1, idmapping2) %>% unique()
 unmatched_uniprot <- am_uniprot %>%
     anti_join(idmapping, by = c("X1" = "uniprot_ID"))
 print(unmatched_uniprot)
-### 215 unmatched
-### Not sure how OP identified the 25 perfectly fine proteins, skipping.
+### 215 unmatched. Not sure how OP identified 25 perfectly fine proteins.
+### skipping.
 am_mapped_to_ensembl_canonical <- am_mapped_to_ensembl_canonical %>%
     filter(!uniprot_ID %in% unmatched_uniprot)
 
-# Check for valid transcripts
+# Check for valid transcripts via CDS
 library(Biostrings)
 fasta <- readDNAStringSet("Homo_sapiens.GRCh38.cds.all.fa.gz")
 ref_df <- tibble(ID = names(fasta), sequence = as.character(fasta))
@@ -127,17 +149,21 @@ for (id in am_mapped_to_ensembl_canonical$ID) {
         invalid_am_ids <- c(invalid_am_ids, id)
     }
 }
-print(invalid_am_ids)
-### 85 transcripts are invalid.
+print(length(invalid_am_ids))
+### 97 transcripts are invalid.
 invalid_am_ids <- c(invalid_am_ids, "ENST00000569103.2")
 ### 1 transcript is a non-functional IG according to OP. Added to invalids.
 
 am_mapped_to_ensembl_canonical <- am_mapped_to_ensembl_canonical %>% filter(!(ID %in% invalid_am_ids))
+write.table(am_mapped_to_ensembl_canonical, "am_mapped_to_ensembl_canonical.txt", sep = "\t", quote = F, row.names = F)
+
 
 # --- Writing output
 
-# AlphaMissense_hg38.tsv.gz -> am_mapped_to_ensembl_canonical -> am_output.tsv
-cat("ID\tchange\tscore\n", file = "am_output.tsv")
+# AlphaMissense_hg38.tsv.gz + AlphaMissense_isoforms_hg38.tsv.gz
+# -> filtered through `am_mapped_to_ensembl_canonical`
+# -> am.tsv
+cat("ID\tchange\tscore\n", file = "am.tsv")
 
 write_chunk <- function(chunk, pos) {
     chunk <- unique(chunk)
@@ -145,55 +171,75 @@ write_chunk <- function(chunk, pos) {
                          "uniprot_ID", "old_ID", "change", "score", "am_class")
     chunk <- chunk %>% filter(uniprot_ID %in% am_mapped_to_ensembl_canonical$uniprot_ID)
     if (nrow(chunk) > 0) {
-        chunk <- left_join(chunk, am_mapped_to_ensembl_canonical, by = c("uniprot_ID" = "uniprot_ID"))
-        subchunk <- select(chunk, "ID", "change", "score")
-        subchunk <- unique(subchunk)
-        write.table(subchunk, "am_output.tsv",
+        chunk <- left_join(chunk, am_mapped_to_ensembl_canonical, by = "uniprot_ID")
+        chunk <- select(chunk, "ID", "change", "score")
+        chunk <- unique(chunk)
+        write.table(chunk, "am.tsv", sep = "\t",
                     col.names = F, row.names = F, quote = F, append = T)
     }
 }
 read_tsv_chunked("AlphaMissense_hg38.tsv.gz",
-                 DataFrameCallback$new(write_chunk),
-                 comment = "#", chunk_size = 1000, show_col_types = F)
-
-
-# AlphaMissense_isoforms_aa_substitutions.tsv.gz -> am_mapped_to_ensembl_canonical -> am_iso_aa_output.tsv
-cat("ID\tchange\tscore\n", file = "am_iso_aa_output.tsv")
-
-write_chunk <- function(chunk, pos) {
-    chunk <- unique(chunk)
-    colnames(chunk) <- c("ID", "change", "score", "am_class")
-    chunk <- chunk %>% mutate(ID2 = sub("\\.\\d+$", "", ID))
-    # match via IDs without version
-    chunk <- chunk %>% filter(ID2 %in% am_mapped_to_ensembl_canonical$ID2)
-    if (nrow(chunk) > 0) {
-        subchunk <- select(chunk, "ID", "change", "score")
-        subchunk <- unique(subchunk)
-        write.table(subchunk, "am_iso_aa_output.tsv",
-                    col.names = F, row.names = F, quote = F, append = T)
-    }
-}
-read_tsv_chunked("AlphaMissense_isoforms_aa_substitutions.tsv.gz",
-                 DataFrameCallback$new(write_chunk),
-                 comment = "#", chunk_size = 1000, show_col_types = F)
-
-
-# AlphaMissense_isoforms_hg38.tsv.gz -> am_mapped_to_ensembl_canonical -> am_iso_output.tsv
-cat("ID\tchange\tscore\n", file = "am_iso_output.tsv")
+                 DataFrameCallback$new(write_chunk), col_names = F,
+                 comment = "#", chunk_size = 10000, show_col_types = F)
 
 write_chunk <- function(chunk, pos) {
     chunk <- unique(chunk)
     colnames(chunk) <- c("CHROM", "POS", "REF", "ALT", "genome",
-                         "ID", "change", "score", "am_class")
-    chunk <- chunk %>% mutate(ID2 = sub("\\.\\d+$", "", ID))
+                         "old_ID", "change", "score", "am_class")
+    chunk <- chunk %>% mutate(ID2 = sub("\\.\\d+$", "", old_ID))
+    # match via IDs without version
     chunk <- chunk %>% filter(ID2 %in% am_mapped_to_ensembl_canonical$ID2)
     if (nrow(chunk) > 0) {
-        subchunk <- select(chunk, "ID", "change", "score")
-        subchunk <- unique(subchunk)
-        write.table(subchunk, "am_iso_output.tsv",
+        chunk <- left_join(chunk, am_mapped_to_ensembl_canonical, by = "ID2")
+        chunk <- select(chunk, "ID", "change", "score")
+        chunk <- unique(chunk)
+        write.table(chunk, "am.tsv", sep = "\t",
                     col.names = F, row.names = F, quote = F, append = T)
     }
 }
 read_tsv_chunked("AlphaMissense_isoforms_hg38.tsv.gz",
-                 DataFrameCallback$new(write_chunk),
-                 comment = "#", chunk_size = 1000, show_col_types = F)
+                 DataFrameCallback$new(write_chunk), col_names = F,
+                 comment = "#", chunk_size = 10000, show_col_types = F)
+
+
+# AlphaMissense_aa_substitutions.tsv.gz + AlphaMissense_isoforms_aa_substitutions.tsv.gz
+# -> filtered through `am_mapped_to_ensembl_canonical`
+# -> am_full.tsv
+cat("ID\tchange\tscore\n", file = "am_full.tsv")
+
+write_chunk <- function(chunk, pos) {
+    chunk <- unique(chunk)
+    colnames(chunk) <- c("uniprot_ID", "change", "score", "am_class")
+    chunk <- chunk %>% filter(uniprot_ID %in% am_mapped_to_ensembl_canonical$uniprot_ID)
+    if (nrow(chunk) > 0) {
+        chunk <- left_join(chunk, am_mapped_to_ensembl_canonical, by = "uniprot_ID")
+        chunk <- select(chunk, "ID", "change", "score")
+        chunk <- unique(chunk)
+        write.table(chunk, "am_full.tsv", sep = "\t",
+                    col.names = F, row.names = F, quote = F, append = T)
+    }
+}
+read_tsv_chunked("AlphaMissense_aa_substitutions.tsv.gz",
+                 DataFrameCallback$new(write_chunk), col_names = F,
+                 comment = "#", chunk_size = 10000, show_col_types = F)
+
+write_chunk <- function(chunk, pos) {
+    chunk <- unique(chunk)
+    colnames(chunk) <- c("old_ID", "change", "score", "am_class")
+    chunk <- chunk %>% mutate(ID2 = sub("\\.\\d+$", "", old_ID))
+    chunk <- chunk %>% filter(ID2 %in% am_mapped_to_ensembl_canonical$ID2)
+    if (nrow(chunk) > 0) {
+        chunk <- left_join(chunk, am_mapped_to_ensembl_canonical, by = "ID2")
+        chunk <- select(chunk, "ID", "change", "score")
+        chunk <- unique(chunk)
+        write.table(chunk, "am_full.tsv", sep = "\t",
+                    col.names = F, row.names = F, quote = F, append = T)
+    }
+}
+read_tsv_chunked("AlphaMissense_isoforms_aa_substitutions.tsv.gz",
+                 DataFrameCallback$new(write_chunk), col_names = F,
+                 comment = "#", chunk_size = 10000, show_col_types = F)
+
+
+# --- Check the lengths
+# TODO
